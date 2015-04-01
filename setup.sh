053585b0214cd92498917fc6cba775eae0dddade
Edward Tan <guofoo@gmail.com>
2015-03-31 17:47:46 -0700

    add commands such as npm link in setup.sh to run once

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+npm link through2
+npm link split
+npm link split
+npm link concat-stream
+npm link http
+npm link request
+npm link stream-combiner
+npm link trumpet
+npm link websocket-stream
+npm link duplexer2
+npm link stream-combiner
+npm link strip
+npm link zlib
+npm link crypto
+npm link tar
+
