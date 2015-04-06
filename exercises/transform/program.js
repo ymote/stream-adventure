e41508b91fe3845da5d575395e950dfbffaaa3ac
Yue Chen <dspfac@gmail.com>
2015-04-06 08:28:22 +0000

    commit exercise content by Live4Code

--- exercises/transform/program.js
+++ exercises/transform/program.js
@@ -2,9 +2,10 @@
   var through = require('through2');
   var tr = through(function (buf, _, next) {
       // enter your code below
-      
+       this.push(buf.toString().toUpperCase());
       next();
   });
 
   process.stdin.pipe(tr).pipe(process.stdout);
+
 
